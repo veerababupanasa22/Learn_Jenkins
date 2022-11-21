92c9a2e461782687a9a6f923928beb90df840dd0
vu <veerababupanasa22@gmail.com>
2022-11-21 18:26:28 +0530

    commit 019

--- JenkinsBuilds/src/com/project/Prog_01.java
+++ JenkinsBuilds/src/com/project/Prog_01.java
@@ -3,12 +3,12 @@ package com.project;
 public class Prog_01 {
     public static void main(String[] args) {
         String str = "Welcome to Javatpoint portal";
-        int count1 = 0;
+        int count2 = 0;
         for (int i=0; i<=str.length()-1; i++) {
             if(str.charAt(i) == 't') {
-                count1++;
+                count2++;
             }
         }
-        System.out.println("Frequency of t is: "+count1);
+        System.out.println("Frequency of t is: "+count2);
     }
 }
